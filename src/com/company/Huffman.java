10880ef1e78fda3856e35bb879300b09b32615c6
ivandamme <imkevandamme@live.nl>
2018-03-17 14:20:07 +0100

    1a en 1b geïmplementeerd

--- src/com/company/Huffman.java
+++ src/com/company/Huffman.java
@@ -1,10 +1,30 @@
 package com.company;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class Huffman {
+    HashMap<Character, Integer> frequentie;
 
-    public List<CharCountHelper> countChars(String toCount){
-        return null;
+    /*
+    Opdracht 1a.
+    Gekozen voor de Hashmap collection omdat hier op een eenvoudige manier de character en het aantal ingezet konden worden.
+    Bij de Hashmap kan je ook op een eenvoudige manier de value van de map ophogen. Bij gebruik van een ArrayList bijv. heb je die mogelijkheid niet.
+
+    */
+    public HashMap<Character, Integer> countChars(String input) {
+
+        frequentie = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            Integer val = frequentie.get(c);
+            if (val != null) {
+                frequentie.put(c, new Integer(val + 1));
+            } else {
+                frequentie.put(c, 1);
+            }
+        }
+
+        return frequentie;
     }
 }
--- test/com/company/HuffmanTest.java
+++ test/com/company/HuffmanTest.java
@@ -1,44 +1,110 @@
 package com.company;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class HuffmanTest {
 
+    /*
+    Opdracht 1a
+
+     */
     @Test
-    public void testCountChars(){
+    public void testCountChars_1() {
+        System.out.println("testCountChars_1");
         Huffman huff = new Huffman();
         String testString1 = "Bananen";
+
+        HashMap<Character, Integer> result1 = huff.countChars(testString1);
+
+        HashMap<Character, Integer> expectedResult1 = new HashMap<Character, Integer>();
+        expectedResult1.put('a', 2);
+        expectedResult1.put('B', 1);
+        expectedResult1.put('e', 1);
+        expectedResult1.put('n', 3);
+
+        // result1 en expectedResult gelijk
+        Assert.assertTrue(result1.equals(expectedResult1));
+
+        expectedResult1.clear();
+
+        expectedResult1.put('a', 2);
+        expectedResult1.put('B', 1);
+        expectedResult1.put('e', 1);
+        expectedResult1.put('n', 7);
+
+        // result en expected result ongelijk
+        Assert.assertTrue(!result1.equals(expectedResult1));
+    }
+
+    @Test
+    public void testCountChars_2() {
+        System.out.println("testCountChars_2");
+        Huffman huff = new Huffman();
         String testString2 = "Laptop";
+
+        HashMap<Character, Integer> result2 = huff.countChars(testString2);
+
+        HashMap<Character, Integer> expectedResult2 = new HashMap<Character, Integer>();
+        expectedResult2.put('L', 1);
+        expectedResult2.put('a', 1);
+        expectedResult2.put('p', 2);
+        expectedResult2.put('t', 1);
+        expectedResult2.put('o', 1);
+
+        // result en expected result gelijk
+        Assert.assertTrue(result2.equals(expectedResult2));
+
+        expectedResult2.clear();
+
+        expectedResult2.put('L', 1);
+        expectedResult2.put('a', 1);
+        expectedResult2.put('p', 2);
+        expectedResult2.put('t', 1);
+        expectedResult2.put('o', 1);
+
+        // result en expected result ongelijk
+        Assert.assertTrue(!result2.equals(expectedResult2));
+    }
+
+    @Test
+    public void testCountChars_3() {
+        System.out.println("testCountChars_3");
+        Huffman huff = new Huffman();
         String testString3 = "Vliegtuig";
 
-        List<CharCountHelper> result1 = huff.countChars(testString1);
-        List<CharCountHelper> result2 = huff.countChars(testString2);
-        List<CharCountHelper> result3 = huff.countChars(testString3);
+        HashMap<Character, Integer> result3 = huff.countChars(testString3);
 
-        List<CharCountHelper> expectedResult1 = new ArrayList<>();
-        expectedResult1.add(new CharCountHelper('a', 2));
-        expectedResult1.add(new CharCountHelper('B', 1));
-        expectedResult1.add(new CharCountHelper('e', 1));
-        expectedResult1.add(new CharCountHelper('n', 3));
+        HashMap<Character, Integer> expectedResult3 = new HashMap<Character, Integer>();
+        expectedResult3.put('V', 1);
+        expectedResult3.put('l', 1);
+        expectedResult3.put('i', 2);
+        expectedResult3.put('e', 1);
+        expectedResult3.put('g', 2);
+        expectedResult3.put('t', 1);
+        expectedResult3.put('u', 1);
 
-        List<CharCountHelper> expectedResult2 = new ArrayList<>();
-        expectedResult2.add(new CharCountHelper('L', 1));
-        expectedResult2.add(new CharCountHelper('a', 1));
-        expectedResult2.add(new CharCountHelper('p', 2));
-        expectedResult2.add(new CharCountHelper('t', 1));
+        // result en expectedResult gelijk
+        Assert.assertTrue(result3.equals(expectedResult3));
 
-        List<CharCountHelper> expectedResult3 = new ArrayList<>();
-        expectedResult3.add(new CharCountHelper('V', 1));
-        expectedResult3.add(new CharCountHelper('l', 1));
-        expectedResult3.add(new CharCountHelper('i', 2));
-        expectedResult3.add(new CharCountHelper('e', 1));
-        expectedResult3.add(new CharCountHelper('g', 2));
-        expectedResult3.add(new CharCountHelper('t', 1));
-        expectedResult3.add(new CharCountHelper('u', 1));
+        expectedResult3.clear();
+        expectedResult3.put('V', 1);
+        expectedResult3.put('l', 1);
+        expectedResult3.put('i', 2);
+        expectedResult3.put('e', 1);
+        expectedResult3.put('g', 2);
+        expectedResult3.put('t', 1);
+        expectedResult3.put('u', 5);
+
+        // result en expected result ongelijk
+        Assert.assertTrue(!result3.equals(expectedResult3));
     }
+
+    /*
+    Einde opdracht 1a.
+     */
 }
